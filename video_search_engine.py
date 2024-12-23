7df4a254456b7dfdfd768a955ca31b66cc2dc93e
Gavriel Cohen <gavrielcohen0314@gmail.com>
2024-12-23 21:04:42 +0200

    Created program that downloads youtube videos

---
+++ video_search_engine.py
@@ -0,0 +1,34 @@
+from yt_dlp import YoutubeDL
+import sys
+
+
+# Progress hook for download
+def progress_hook(d):
+    if d['status'] == 'downloading':
+        sys.stdout.write(f"\r{d.get('downloaded_bytes', 0)}")
+        sys.stdout.flush()
+    elif d['status'] == 'finished':
+        print("\nDownload complete!")
+
+
+def download_video(query):
+
+    ydl_opts = {
+        'format': 'mp4',
+        'outtmpl': 'video.mp4',
+        'default_search': 'ytsearch1',  # Use ytsearch1 to search and download the first result
+        'no_warnings': True,
+        'progress_hooks': [progress_hook],
+        'quiet': True
+    }
+
+    with YoutubeDL(ydl_opts) as ydl:
+        ydl.download([query])
+
+
+if __name__ == "__main__":
+    try:
+        print("Downloading the video from YouTube...")
+        download_video("super mario movie trailer")
+    except Exception as e:
+        print(f"Error downloading video: {e}")
