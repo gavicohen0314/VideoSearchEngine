bdd8e4a1d13b5426aaa5ce7bb9edf2caf32c63e3
Gavriel Cohen <gavrielcohen0314@gmail.com>
2024-12-24 17:32:38 +0200

    Added optimized scene detect function

--- video_search_engine.py
+++ video_search_engine.py
@@ -1,8 +1,12 @@
 from yt_dlp import YoutubeDL
 import sys
 import logging
-from scene_detector import run_scene_detection
+import os
+import shutil
+from scenedetect import SceneManager, AdaptiveDetector, save_images, open_video
 
+VIDEO_PATH = 'video.mp4'
+SCENES_PATH = 'scenes'
 # Configure logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -16,7 +20,7 @@ def progress_hook(d):
         sys.stdout.write(f"\r{d.get('downloaded_bytes', 0)}")
         sys.stdout.flush()
     elif d['status'] == 'finished':
-        logger.info("Download complete!")
+        logger.info("\nDownload complete!")
 
 
 def download_video(query):
@@ -25,7 +29,7 @@ def download_video(query):
     """
     ydl_opts = {
         'format': 'mp4',
-        'outtmpl': 'video.mp4',
+        'outtmpl': VIDEO_PATH,
         'default_search': 'ytsearch1',  # Use ytsearch1 to search and download the first result
         'no_warnings': True,
         'progress_hooks': [progress_hook],
@@ -36,6 +40,25 @@ def download_video(query):
         ydl.download([query])
 
 
+def detect_scenes():
+    if os.path.isdir(SCENES_PATH):
+        logger.info(f"Deleting old output directory: {SCENES_PATH}")
+        shutil.rmtree(SCENES_PATH)
+    video = open_video(VIDEO_PATH)
+    scene_manager = SceneManager()
+    detector = AdaptiveDetector()
+    scene_manager.add_detector(detector)
+    scene_manager.detect_scenes(video)
+    scene_list = scene_manager.get_scene_list()
+    save_images(
+        scene_list,
+        video,
+        num_images=1,
+        output_dir='scenes',
+        image_name_template="${SCENE_NUMBER}"
+    )
+
+
 def main():
     try:
         logger.info("Downloading the video from YouTube...")
@@ -43,6 +66,7 @@ def main():
     except Exception as e:
         logger.error(f"Error downloading video: {e}")
         return
+    detect_scenes()
 
 
 if __name__ == "__main__":
